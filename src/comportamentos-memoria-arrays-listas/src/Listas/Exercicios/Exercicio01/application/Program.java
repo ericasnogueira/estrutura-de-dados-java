150b66552b079f0a9be0243cdbca9262108e3901
erica nogueira <ericasnogueira10@gmail.com>
2022-11-14 15:54:32 -0300

    Exercicio parte 01

---
+++ src/comportamentos-memoria-arrays-listas/src/Listas/Exercicios/Exercicio01/application/Program.java
@@ -0,0 +1,17 @@
+package Listas.Exercicios.Exercicio01.application;
+
+public class Program {
+    public static void main(String[] args) {
+        /*
+        Fazer um programa para ler um número inteiro N e depois os dados(ID, nome e salario) de N funcionarios. Não
+        deve haver repetição de Id.
+
+        Em seguida, efetuar o aumento de X por cento no salário de um determidado funcionário. Para isso, o programa
+        deve ler um id e o valor X. Se o ID informado não exixtir, mostrar uma mensagem e abortar a operação. Ao
+        final, mostrar a listagem atualizada dos funcionarios, conforme exemplos.
+
+        Lembre-se de aplicar a técnica de encapsulamento para não permitir que salário possa ser mudado livremente. Um
+        salário só pode ser aumentado com base em uma operação de aumento por porcentagem ddada.
+         */
+    }
+}
