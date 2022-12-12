211adde5cfa1b6ce234ba882b5e285f8390a56f8
erica nogueira <ericasnogueira10@gmail.com>
2022-12-12 10:22:59 -0300

    List

--- src/Arrays_e_Collections/Colecoes/Lista/Lista.java
+++ src/Arrays_e_Collections/Colecoes/Lista/Lista.java
@@ -1,2 +1,40 @@
-package Arrays_e_Collections.Colecoes.Lista;public class Lista {
+package Arrays_e_Collections.Colecoes.Lista;
+
+import java.util.ArrayList;
+
+public class Lista {
+    public static void main(String[] args) {
+
+        ArrayList<Usuario> lista = new ArrayList<>();
+
+        Usuario u1 = new Usuario("Ana");
+
+        //adicionando  u1 na lista
+        lista.add(u1);// assim pode
+
+        //lista.add("Erica");// assim não vai
+
+        lista.add(new Usuario("Erica"));// assim pode
+        lista.add(new Usuario("Lia"));// assim pode
+        lista.add(new Usuario("Bia"));// assim pode
+        lista.add(new Usuario("Manu"));// assim pode
+        lista.add(new Usuario("Carlos"));// assim pode
+
+        //percorrendo a lista
+        //  tipo // variavel :  // nome da lista
+        for(Usuario u: lista){
+            System.out.println(u);
+        }
+
+        System.out.println("========");
+        //obtendo um determinado elemento pelo id
+        System.out.println(lista.get(3));//acessando  pelo índice
+        System.out.println("/////////");
+        //removendo
+        lista.remove(1);
+        lista.remove(new Usuario("Carlos"));
+        for(Usuario u: lista){
+            System.out.println(u);
+        }
+    }
 }
--- src/Arrays_e_Collections/Colecoes/Lista/Usuario.java
+++ src/Arrays_e_Collections/Colecoes/Lista/Usuario.java
@@ -1,2 +1,34 @@
-package Arrays_e_Collections.Colecoes.Lista;public class Usuario {
+package Arrays_e_Collections.Colecoes.Lista;
+
+import java.util.Objects;
+
+public class Usuario {
+    String nome;
+
+    //contrutor
+    public Usuario(String nome) {
+        this.nome = nome;
+    }
+    //toString
+
+    @Override
+    public String toString() {
+        return " meu nome é " + nome ;
+    }
+
+
+    //gerando o hashCode e equals
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Usuario usuario = (Usuario) o;
+        return nome.equals(usuario.nome);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(nome);
+    }
 }
